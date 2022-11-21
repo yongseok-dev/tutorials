30426d1bcddaa9a6abcf361cf7ca5175b76093e6
yongseok <yongseok.dev@gmail.com>
2022-11-22 01:09:22 +0900

    webRTC 브라우저에 카메라 띄우기

--- video/webRTC/front/src/App.js
+++ video/webRTC/front/src/App.js
@@ -1,7 +1,31 @@
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
-}
+  const localVideoRef = useRef(null);
 
+  const getLocalVideo = async () => {
+    const mediaStream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    localVideoRef.current.srcObject = mediaStream;
+  };
+
+  useEffect(() => {
+    localVideoRef.current.onloadedmetadata = () => {
+      localVideoRef.current.play();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {' '}
+        <video id="localVideo" ref={localVideoRef} muted />
+        <button onClick={getLocalVideo}>카메라 가져오기</button>
+      </header>
+    </div>
+  );
+}
 export default App;
