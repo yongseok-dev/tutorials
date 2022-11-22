30b362d867e49099298d7523aaf8d34a77013c1e
yongseok <yongseok.dev@gmail.com>
2022-11-22 13:31:08 +0900

    ssl test code

--- video/webRTC/back/ssl.js
+++ video/webRTC/back/ssl.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import https from 'https';
+import fs from 'fs';
 
 const options = {
-  key: fs.readFileSync(path.join('cert/private.key')),
-  cert: fs.readFileSync(path.join('cert/certificate.crt')),
+  key: fs.readFileSync('./cert/private.key'),
+  cert: fs.readFileSync('./cert/certificate.crt'),
 };
 
 const app = express();
+
+const server = https.createServer(options, app);
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello SSL</h1>');
 });
 
-https.createServer(options, app).listen(443, () => {
-  console.log('443 port open');
+// START THE SERVER =================================================================
+const port = 443;
+server.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
 });
