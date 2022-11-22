79ffeaff7470f87d81814ea7661423ac56f9aae7
yongseok <yongseok.dev@gmail.com>
2022-11-22 12:02:15 +0900

    webRTC 소켓연결 및 SDP 전달 확인

--- video/webRTC/back/app.js
+++ video/webRTC/back/app.js
@@ -14,8 +14,34 @@ const io = new Server(server, {
   },
 });
 
+const offerMap = new Map();
+
 io.on('connection', (socket) => {
   console.log(`socket connected: ${socket.id}`);
+  //1.client =>join(roomId) caller.req
+  socket.on('join', ({ roomId }) => {
+    console.log(`join-room-id: ${roomId}`);
+    socket.join(roomId);
+
+    //방이 있는지 체크
+    const prevOffer = offerMap.get(roomId);
+    // console.log(`prevOffer: `, prevOffer);
+
+    //2.client <= remote-offer(offer)
+    socket.emit('remote-offer', { offer: prevOffer });
+  });
+
+  //3-1.client => new-offer(offer,roomID)
+  socket.on('new-offer', ({ offer, roomId }) => {
+    offerMap.set(roomId, offer);
+  });
+
+  //3-2.client => new-answer(offer,roomID)
+  socket.on('new-answer', ({ answer, roomId }) => {
+    //4.clint <= remote-answer(answer)
+    console.log(`4.clint <= remote-answer(answer)`, answer);
+    socket.to(roomId).emit('remote-answer', { answer });
+  });
 });
 
 app.use(
--- video/webRTC/front/src/App.js
+++ video/webRTC/front/src/App.js
@@ -2,29 +2,84 @@ import { useEffect, useRef } from 'react';
 import './App.css';
 import { io } from 'socket.io-client';
 
-// const configuration = {
-//   iceServers: [
-//     {
-//       urls: [
-//         'stun:stun.l.google.com:19302',
-//         'stun:stun1.l.google.com:19302',
-//         'stun:stun2.l.google.com:19302',
-//         'stun:stun3.l.google.com:19302',
-//         'stun:stun4.l.google.com:19302',
-//       ],
-//     },
-//   ],
-// };
+const configuration = {
+  iceServers: [
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+        'stun:stun4.l.google.com:19302',
+      ],
+    },
+  ],
+};
 
 function App() {
   const localVideoRef = useRef(null);
-  // const roomIdRef = useRef('randomString');
-  // const pcRef = useRef(new RTCPeerConnection(configuration));
+  const roomIdRef = useRef('randomString');
+  const pcRef = useRef(new RTCPeerConnection(configuration));
+  const socketRef = useRef(null);
+
+  const createNewOffer = async () => {
+    console.log('roomId가 없음');
+    const newOffer = await pcRef.current.createOffer();
+    console.log(`>> newOffer`, newOffer);
+    await pcRef.current.setLocalDescription(newOffer);
+    //new-offer(offer,roomID) => 3-1.socket
+    socketRef.current.emit('new-offer', {
+      offer: newOffer,
+      roomId: roomIdRef.current,
+    });
+  };
+
+  const handleRemoteAnswer = async (answer) => {
+    const remoteAnswer = new RTCSessionDescription(answer);
+    console.log(`remoteAnswer`, remoteAnswer);
+    await pcRef.setRemoteDescription(remoteAnswer);
+  };
+
+  const handleRemoteOffer = async (offer) => {
+    console.log('>> roomId가 있음');
+    const remoteOffer = new RTCSessionDescription(offer);
+    console.log(`remoteOffer: `, remoteOffer);
+    await pcRef.current.setRemoteDescription(remoteOffer);
+    const newAnswer = await pcRef.current.createAnswer(remoteOffer);
+    console.log(`newAnswer: `, newAnswer);
+    await pcRef.current.setLocalDescription(newAnswer);
+
+    //new-answer(offer,roomID) => 3-2.socket
+    socketRef.current.emit('new-answer', {
+      answer: newAnswer,
+      roomId: roomIdRef.current,
+    });
+  };
 
   const handleJoinBtnClick = () => {
-    console.log('bt click');
+    console.log('>> Join Click');
+    //socket connect
     const socket = io('http://localhost:8000');
-    console.log(`socket: ${socket}`);
+    socketRef.current = socket;
+    console.log(`socket: `, socket);
+    //join(roomID) => 1.socket
+    socket.emit('join', { roomId: roomIdRef.current });
+    //remote-offer(offer) <= 2.socket
+    socket.on('remote-offer', ({ offer }) => {
+      console.log(`<< remote-offer'`, offer);
+      if (offer) {
+        handleRemoteOffer(offer); //roomId 보유 (3-1)
+        return;
+      }
+
+      createNewOffer(); //roomId 미보유 (3-2)
+    });
+
+    //remote-answer(answer) <= 4.socket
+    socket.on('remote-answer', ({ answer }) => {
+      console.log('<< remote-answer', answer);
+      handleRemoteAnswer(answer);
+    });
   };
 
   const getLocalVideo = async () => {
